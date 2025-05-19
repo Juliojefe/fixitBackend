13af836a4782049d1ab72a56b05d3863466f5a29
Juliojefe <j.c.jf285@gmail.com>
2025-05-19 18:49:46 +0100

    empty backend

---
+++ backend/src/test/java/com/example/fixit/FixitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fixit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
